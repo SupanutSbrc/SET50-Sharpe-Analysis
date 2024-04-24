54e691d5468d1bb32568a1ae73b6dc02a0641b54
Supanut Sriburachai <147172225+SupanutSbrc@users.noreply.github.com>
2024-04-24 15:30:34 +0700

    Add files via upload

--- gt.py
+++ gt.py
@@ -1,30 +1,30 @@
-interval = ['1Y-sharpe', '3Y-sharpe', '5Y-sharpe', '10Y-sharpe']
-r_interval = ['1Y-avg_r','3Y-avg_r','5Y-avg_r','10Y-avg_r']
-std_interval = ['1Y-std','3Y-std','5Y-std','10Y-std']
-conclu = pd.DataFrame(name, columns=['SET50'])
-for j in range (len(interval)):
-    if j == 0 :
-        wo = 12
-    elif j == 1:
-        wo = 36
-    elif j == 2:
-        wo = 60
-    else:
-        wo = 120
-    mean_list = []
-    std_list = []
-    for k in range (len(name)):
-        data = df_return[name[k]].iloc[-wo:]
-        mean = data.mean() * 12
-        mean_list.append(mean)
-        std = data.std() * sqrt(12)
-        std_list.append(std)
-    conclu['rrf'] = rf
-    if data.isnull().values.any():
-        conclu[r_interval[j]] = np.nan
-        conclu[std_interval[j]] = np.nan
-        conclu[interval[j]] = np.nan
-    else:
-        conclu[r_interval[j]] = np.array(mean_list)
-        conclu[std_interval[j]] = np.array(std_list)
-        conclu[interval[j]] = (conclu[r_interval[j]]-conclu['rrf'])/conclu[std_interval[j]]
+from tvDatafeed import TvDatafeed, Interval
+import pandas as pd
+import numpy as np
+from math import sqrt
+
+tv = TvDatafeed()
+
+df = tv.get_hist(symbol='SET:SET50', exchange='SET', interval=Interval.in_daily, n_bars=4500)
+from statsmodels.tsa.stattools import adfuller
+result = adfuller(df['close'], autolag='BIC')
+print(result[1])
+returns = np.log(df['close']).diff().dropna() #1st difference
+
+result = adfuller(returns, autolag='BIC')
+print(result[4])
+print(result[0])
+print(result[1])
+returns_train, returns_test= returns[:-120], returns[-120:]
+from arch import arch_model
+garch_model_fit = arch_model(returns_train,vol="Garch",p=1,q=1).fit(disp='off')
+print(garch_model_fit.summary)
+arch_model_fit = arch_model(returns_train,vol="ARCH").fit(disp='off')
+print(arch_model_fit.summary)
+egarch_model_fit = arch_model(returns_train,vol="EGARCH",p=1,q=1).fit(disp='off')
+print(egarch_model_fit.summary)
+
+realized_volatility = np.log(df['high']/df['low'])
+realized_volatility = realized_volatility.iloc[1:]
+realized_volatility = realized_volatility
+realized_volatility_train, realized_volatility_test= realized_volatility[:-120], realized_volatility[-120:]
