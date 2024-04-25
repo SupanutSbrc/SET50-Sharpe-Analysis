5465ecdd4018d3e73980de31c559105642833bff
Supanut Sriburachai <147172225+SupanutSbrc@users.noreply.github.com>
2024-04-25 16:15:30 +0700

    Update gt.py

--- gt.py
+++ gt.py
@@ -1,30 +1,20 @@
-from tvDatafeed import TvDatafeed, Interval
-import pandas as pd
-import numpy as np
-from math import sqrt
-
-tv = TvDatafeed()
-
-df = tv.get_hist(symbol='SET:SET50', exchange='SET', interval=Interval.in_daily, n_bars=4500)
-from statsmodels.tsa.stattools import adfuller
-result = adfuller(df['close'], autolag='BIC')
-print(result[1])
-returns = np.log(df['close']).diff().dropna() #1st difference
-
-result = adfuller(returns, autolag='BIC')
-print(result[4])
-print(result[0])
-print(result[1])
-returns_train, returns_test= returns[:-120], returns[-120:]
-from arch import arch_model
-garch_model_fit = arch_model(returns_train,vol="Garch",p=1,q=1).fit(disp='off')
-print(garch_model_fit.summary)
-arch_model_fit = arch_model(returns_train,vol="ARCH").fit(disp='off')
-print(arch_model_fit.summary)
-egarch_model_fit = arch_model(returns_train,vol="EGARCH",p=1,q=1).fit(disp='off')
-print(egarch_model_fit.summary)
-
-realized_volatility = np.log(df['high']/df['low'])
-realized_volatility = realized_volatility.iloc[1:]
-realized_volatility = realized_volatility
-realized_volatility_train, realized_volatility_test= realized_volatility[:-120], realized_volatility[-120:]
+for loop in range(3,120):
+    df_X = df_return.copy()
+    df_X = df_X[['set50']]
+    df_X['set50'] = df_X['set50'].rolling(window=3).std()
+    for i in range(len(name)):
+        df_X[name[i]] = df_return[name[i]].rolling(window=3).std()
+    df_X_shifted = df_X.shift(120)
+    df_X_shifted
+    stacked_X_shifted = df_X_shifted.stack(dropna=False).reset_index(drop=True)
+    stacked_X_shifted
+    newdf = pd.concat([stacked_X_shifted,stacked_y],axis=1, keys=['X','Y'])
+    newdf = newdf.dropna()
+    Y = newdf['Y']
+    X= newdf['X']
+    X = sm.add_constant(X)
+    model = sm.OLS(Y,X)
+    result = model.fit()
+    newdf['Yhat'] = result.predict(X)
+    mae = mean_absolute_error(newdf['Y'],newdf['Yhat'])
+    print(mae)
