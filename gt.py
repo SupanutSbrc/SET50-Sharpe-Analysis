86fc3ee149a0762ea1d4ed8475b01b29b3d761db
Supanut Sriburachai <147172225+SupanutSbrc@users.noreply.github.com>
2024-04-25 16:18:13 +0700

    Update gt.py

--- gt.py
+++ gt.py
@@ -1,9 +1,9 @@
 for loop in range(3,120):
     df_X = df_return.copy()
     df_X = df_X[['set50']]
-    df_X['set50'] = df_X['set50'].rolling(window=3).std()
+    df_X['set50'] = df_X['set50'].rolling(window=loop).std()
     for i in range(len(name)):
-        df_X[name[i]] = df_return[name[i]].rolling(window=3).std()
+        df_X[name[i]] = df_return[name[i]].rolling(window=loop).std()
     df_X_shifted = df_X.shift(120)
     df_X_shifted
     stacked_X_shifted = df_X_shifted.stack(dropna=False).reset_index(drop=True)
