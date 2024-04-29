0f7e38a646092804372448e4cef71d3d0c5cf6d4
Supanut Sriburachai <147172225+SupanutSbrc@users.noreply.github.com>
2024-04-29 11:22:33 +0700

    Update gt.py

--- gt.py
+++ gt.py
@@ -1,20 +1,6 @@
-for loop in range(3,120):
-    df_X = df_return.copy()
-    df_X = df_X[['set50']]
-    df_X['set50'] = df_X['set50'].rolling(window=loop).std()
-    for i in range(len(name)):
-        df_X[name[i]] = df_return[name[i]].rolling(window=loop).std()
-    df_X_shifted = df_X.shift(120)
-    df_X_shifted
-    stacked_X_shifted = df_X_shifted.stack(dropna=False).reset_index(drop=True)
-    stacked_X_shifted
-    newdf = pd.concat([stacked_X_shifted,stacked_y],axis=1, keys=['X','Y'])
-    newdf = newdf.dropna()
-    Y = newdf['Y']
-    X= newdf['X']
-    X = sm.add_constant(X)
-    model = sm.OLS(Y,X)
-    result = model.fit()
-    newdf['Yhat'] = result.predict(X)
-    mae = mean_absolute_error(newdf['Y'],newdf['Yhat'])
-    error.append(mae)
+for i in range (len(name)):
+    df = tv.get_hist(symbol=name[i], exchange='HKEX', interval=Interval.in_daily, n_bars=961)
+    df = df[['close']]
+    print(len(df))
+    hk50 = pd.merge(hk50, df, left_index=True, right_index=True, how='outer')
+    hk50.rename(columns = {'close':name[i]}, inplace=True)
