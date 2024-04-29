e28a277e7070dc45e9e1d90618c42397ae77ff0d
Supanut Sriburachai <147172225+SupanutSbrc@users.noreply.github.com>
2024-04-29 11:53:42 +0700

    Update gt.py

--- gt.py
+++ gt.py
@@ -1,6 +1,44 @@
+best_model_list = []
 for i in range (len(name)):
-    df = tv.get_hist(symbol=name[i], exchange='HKEX', interval=Interval.in_daily, n_bars=961)
-    df = df[['close']]
-    print(len(df))
-    hk50 = pd.merge(hk50, df, left_index=True, right_index=True, how='outer')
-    hk50.rename(columns = {'close':name[i]}, inplace=True)
+    returns_individual = returns[[name[i]]]
+    returns_individual = returns_individual.dropna()
+    len_test = int(0.2 * len(returns_individual))
+    test_vol = real_vol[-len_test:]
+    returns_train, returns_test= returns_individual[:-len_test], returns_individual[-len_test:]
+    garch_model_fit = arch_model(returns_train,vol="Garch",p=1,q=1).fit(disp='off',update_freq=len(returns_test))
+    arch_model_fit = arch_model(returns_train,vol="ARCH").fit(disp='off',update_freq=len(returns_test))
+    gjr_garch_model_fit = arch_model(returns_train,p=1,o=1,q=1).fit(disp='off',update_freq=len(returns_test))
+    harch_model_fit = arch_model(returns_train,vol='Harch').fit(disp='off',update_freq=len(returns_test))
+
+    garch_forecast = garch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
+    arch_forecast = arch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
+    gjr_garch_forecast = gjr_garch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
+    harch_forecast = harch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
+    garch = garch_forecast.variance.values[-1,:].flatten()
+    arch = arch_forecast.variance.values[-1,:].flatten()
+    gjr = gjr_garch_forecast.variance.values[-1,:].flatten()
+    harch = harch_forecast.variance.values[-1,:].flatten()
+    df_test = returns_test.to_frame(name='return')
+    df_test['garch']= np.sqrt(garch)
+    df_test['arch']= np.sqrt(arch)
+    df_test['gjr']= np.sqrt(gjr)
+    df_test['harch']= np.sqrt(harch)
+    df_test['Vol'] = np.array(test_vol)
+
+    garch_mae = mean_absolute_error(df_test['Vol'],df_test['garch'])
+    arch_mae = mean_absolute_error(df_test['Vol'],df_test['arch'])
+    gjr_garch_mae = mean_absolute_error(df_test['Vol'],df_test['gjr'])
+    harch_mae = mean_absolute_error(df_test['Vol'],df_test['harch'])
+    mae = [garch_mae,arch_mae,gjr_garch_mae,harch_mae]
+    best = mae.index(min(mae))
+
+    if best == 0:
+        mod = 'garch'
+    elif best == 1:
+        mod = 'arch'
+    elif best == 2:
+        mod = 'gjr'
+    elif best == 3:
+        mod = 'harch'
+    best_model_list.append(mod)
+best_model_list
