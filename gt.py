408d7d4dcc3d6c2a9cb27868900a2de813822f5b
Supanut Sriburachai <147172225+SupanutSbrc@users.noreply.github.com>
2024-04-30 11:29:38 +0700

    Update gt.py

--- gt.py
+++ gt.py
@@ -1,44 +1,19 @@
-best_model_list = []
-for i in range (len(name)):
-    returns_individual = returns[[name[i]]]
-    returns_individual = returns_individual.dropna()
-    len_test = int(0.2 * len(returns_individual))
-    test_vol = real_vol[-len_test:]
-    returns_train, returns_test= returns_individual[:-len_test], returns_individual[-len_test:]
-    garch_model_fit = arch_model(returns_train,vol="Garch",p=1,q=1).fit(disp='off',update_freq=len(returns_test))
-    arch_model_fit = arch_model(returns_train,vol="ARCH").fit(disp='off',update_freq=len(returns_test))
-    gjr_garch_model_fit = arch_model(returns_train,p=1,o=1,q=1).fit(disp='off',update_freq=len(returns_test))
-    harch_model_fit = arch_model(returns_train,vol='Harch').fit(disp='off',update_freq=len(returns_test))
+import requests
+from bs4 import BeautifulSoup
+url = 'https://www.thaiwarrant.com/dw/search'
+head = Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36
 
-    garch_forecast = garch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
-    arch_forecast = arch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
-    gjr_garch_forecast = gjr_garch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
-    harch_forecast = harch_model_fit.forecast(horizon=len(returns_test),method='analytic',reindex=True)
-    garch = garch_forecast.variance.values[-1,:].flatten()
-    arch = arch_forecast.variance.values[-1,:].flatten()
-    gjr = gjr_garch_forecast.variance.values[-1,:].flatten()
-    harch = harch_forecast.variance.values[-1,:].flatten()
-    df_test = returns_test.to_frame(name='return')
-    df_test['garch']= np.sqrt(garch)
-    df_test['arch']= np.sqrt(arch)
-    df_test['gjr']= np.sqrt(gjr)
-    df_test['harch']= np.sqrt(harch)
-    df_test['Vol'] = np.array(test_vol)
-
-    garch_mae = mean_absolute_error(df_test['Vol'],df_test['garch'])
-    arch_mae = mean_absolute_error(df_test['Vol'],df_test['arch'])
-    gjr_garch_mae = mean_absolute_error(df_test['Vol'],df_test['gjr'])
-    harch_mae = mean_absolute_error(df_test['Vol'],df_test['harch'])
-    mae = [garch_mae,arch_mae,gjr_garch_mae,harch_mae]
-    best = mae.index(min(mae))
-
-    if best == 0:
-        mod = 'garch'
-    elif best == 1:
-        mod = 'arch'
-    elif best == 2:
-        mod = 'gjr'
-    elif best == 3:
-        mod = 'harch'
-    best_model_list.append(mod)
-best_model_list
+print('0')
+response = requests.get(url, headers={Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36})
+print('yes')
+if response.status_code == 200:
+    print('1')
+    bs = BeautifulSoup(url,'html.parser')
+    print('2')
+    table = bs.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="MainContent_gvDWSearch")
+    print('3')
+    rows = table.findAll(lambda tag: tag.name=='tr')
+    print('4')
+    rows
+else:
+    print("failed")
