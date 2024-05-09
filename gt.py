a67538080cb8ce2d2f7955f25c3d7183e937b9f2
Supanut Sriburachai <147172225+SupanutSbrc@users.noreply.github.com>
2024-05-09 16:29:29 +0700

    Update gt.py

--- gt.py
+++ gt.py
@@ -1,18 +1,55 @@
-from tvDatafeed import TvDatafeed, Interval
-import pandas as pd
-import numpy as np
-from math import sqrt
-import matplotlib.pyplot as plt
+Sub MergeDataByMatchingColumns()
 
-tv = TvDatafeed()
-df = tv.get_hist(symbol='SET:SET50', exchange='SET', interval=Interval.in_daily, n_bars=1201)
-set50 = df[['close']]
-set50.rename(columns = {'close':'remove'}, inplace=True)
+Dim wsOutstanding As Worksheet, wsPortfolio As Worksheet, wsMerged As Worksheet
+Dim dataOutstanding As Variant, dataPortfolio As Variant
+Dim matchCol11 As Long, matchCol12 As Long, matchCol21 As Long, matchCol22 As Long
+Dim lastRow1 As Long, lastRow2 As Long
+Dim i As Long, j As Long, k As Long
 
-for i in range (df_dw01.shape[0]):
-    df = tv.get_hist(symbol=df['Stock'][i], exchange='SET', interval=Interval.in_daily, n_bars=1201)
-    df = df[['close']]
-    set50 = pd.merge(set50, df, left_index=True, right_index=True, how='outer')
-    set50.rename(columns = {'close':df['Stock'][i]}, inplace=True)
-set50 = set50.drop('remove',axis=1)
-set50
+' Define worksheets
+Set wsOutstanding = ThisWorkbook.Worksheets("OutstandingReport")
+Set wsPortfolio = ThisWorkbook.Worksheets("portfolioDataLive")
+
+' Define matching columns (1-based index)
+' In OutstandingReport
+matchCol11 = wsOutstanding.Cells(1, Columns.Count).End(xlToLeft).Column ' Last column containing data (ELN No)
+matchCol12 = 1 ' Assuming UL Name is in the first column
+
+' In portfolioDataLive
+matchCol21 = wsPortfolio.Cells(1, Columns.Count).End(xlToLeft).Column ' Last column containing data (CC Underlying Name)
+matchCol22 = 3 ' Assuming CC ELN No is in the third column (adjust based on your actual data)
+
+' Get data from worksheets
+lastRow1 = wsOutstanding.Cells(Rows.Count, 1).End(xlUp).Row
+lastRow2 = wsPortfolio.Cells(Rows.Count, 1).End(xlUp).Row
+dataOutstanding = wsOutstanding.Range(wsOutstanding.Cells(2, 1), wsOutstanding.Cells(lastRow1, matchCol11)).Value  ' Capture relevant columns from OutstandingReport
+dataPortfolio = wsPortfolio.Range(wsPortfolio.Cells(2, 1), wsPortfolio.Cells(lastRow2, matchCol21)).Value  ' Capture relevant columns from portfolioDataLive
+
+' Find matching rows
+Dim mergedData As Variant
+ReDim mergedData(1 To UBound(dataOutstanding, 1) + UBound(dataPortfolio, 1), UBound(dataOutstanding, 2) + UBound(dataPortfolio, 2))
+
+k = 1
+For i = 1 To UBound(dataOutstanding, 1)
+    For j = 1 To UBound(dataPortfolio, 1)
+        If dataOutstanding(i, matchCol11) = dataPortfolio(j, matchCol22) And dataOutstanding(i, matchCol12) = dataPortfolio(j, matchCol21) Then  ' Check for both ELN No and UL Name match
+            For m = 1 To UBound(dataOutstanding, 2)
+                mergedData(k, m) = dataOutstanding(i, m)
+            Next m
+            For m = UBound(dataOutstanding, 2) + 1 To UBound(dataPortfolio, 2)
+                mergedData(k, m) = dataPortfolio(j, m)
+            Next m
+            k = k + 1
+            Exit For ' Exit inner loop after finding a match
+        End If
+    Next j
+Next i
+
+' Create new worksheet for merged data
+Set wsMerged = ThisWorkbook.Worksheets.Add
+wsMerged.Name = "Merged Table"  ' Set the name of the new worksheet
+
+' Write merged data to the new worksheet
+wsMerged.Range("A1").Resize(UBound(mergedData, 1), UBound(mergedData, 2)).Value = mergedData
+
+End Sub
